9ca63714e8142491b7021cb4f08de27e70138866
kirantidke <kirantidke2013@gmail.com>
2021-07-06 11:06:23 +0530

    [kiran]REFACTORED:code to add new Address Book using stream

--- src/main/java/com/bridgelabz/addressbook/Contacts.java
+++ src/main/java/com/bridgelabz/addressbook/Contacts.java
@@ -1,46 +1,38 @@
 package com.bridgelabz.addressbook;
 
 public class Contacts {
-    private String fName;
-    private String lName;
-    private String address;
-    private String city;
-    private String state;
-    private Long zip;
-    private Long phone;
-    private String email;
+	private String fName;
+	private String lName;
+	private String address;
+	private String city;
+	private String state;
+	private Long zip;
+	private Long phone;
+	private String email;
 
-    public Contacts(String fName, String lName, String address, String city, String state, Long zip, Long phone,
-                    String email) {
-        this.fName = fName;
-        this.lName = lName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email;
-    }
+	public Contacts(String fName, String lName, String address, String city, String state, Long zip, Long phone,
+			String email) {
+		this.fName = fName;
+		this.lName = lName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+	}
 
-    @Override
-    public String toString() {
-        return
-                "\nFirst_Name  : " + fName +
-                "\nLast_Name   : " + lName +
-                "\nAddress     : " + address +
-                "\nCity        : " + city +
-                "\nState       : " + state +
-                "\nZIP Code    : " + zip +
-                "\nPhone_Number: " + phone +
-                "\nEmail_ID    : " + email +
-                "\n-----------------------------------";
-    }
+	@Override
+	public String toString() {
+		return "\nFirst_Name  : " + fName + "\nLast_Name   : " + lName + "\nAddress     : " + address
+				+ "\nCity        : " + city + "\nState       : " + state + "\nZIP Code    : " + zip + "\nPhone_Number: "
+				+ phone + "\nEmail_ID    : " + email + "\n-----------------------------------";
+	}
+	public String getfName() {
+		return fName;
+	}
 
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
 }

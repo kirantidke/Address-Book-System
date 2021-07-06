b34465d63fc63036750ade40e69867358c03e4cf
kirantidke <kirantidke2013@gmail.com>
2021-07-06 09:02:30 +0530

    [kiran]ADDED:addredss book contact

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.addressbook;
+import java.util.Scanner;
+
+public class AddressBook {
+	Contacts contact;
+	Scanner sc = new Scanner(System.in);
+
+	public void add() {
+
+		contact = new Contacts();
+		
+		//Takes input from user
+		
+		System.out.println("Enter First Name: ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter last Name: ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter your address: ");
+		String address = sc.nextLine();
+		System.out.println("Enter your city: ");
+		String city = sc.nextLine();
+		System.out.println("Enter your state: ");
+		String state = sc.nextLine();
+		System.out.println("Enter zip code of your area: ");
+		Long zipCode = sc.nextLong();
+		sc.nextLine();
+		System.out.println("Enter phone number: ");
+		Long phoneNum = sc.nextLong();
+		sc.nextLine();
+		System.out.println("Enter your EMail ID: ");
+		String email = sc.nextLine();
+		
+		//calling the set method
+		
+		contact.setFirstName(firstName);
+		contact.setLastName(lastName);
+		contact.setAddress(address);
+		contact.setCity(city);
+		contact.setState(state);
+		contact.setZipCode(zipCode);
+		contact.setPhoneNumber(phoneNum);
+		contact.setEmail(email);
+		System.out.println(contact);
+	}
+}
--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.addressbook;
 
 /**
- * Hello world!
+ * Welcome!
  *
  */
 

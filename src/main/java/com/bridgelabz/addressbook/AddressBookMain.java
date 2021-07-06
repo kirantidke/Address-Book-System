fe2779836e0c550af3c62b20c87569e81a742b96
kirantidke <kirantidke2013@gmail.com>
2021-07-06 08:51:49 +0530

    [kiran]ADDED:Contacts to Address Book

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -4,10 +4,8 @@ package com.bridgelabz.addressbook;
  * Hello world!
  *
  */
-public class AddressBookMain 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Address Book System!" );
-    }
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System!");
+	}
 }
